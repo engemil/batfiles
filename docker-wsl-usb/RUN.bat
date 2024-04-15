4b7c6ed7f699fbb93d476c33174a38d85bc86129
Emil Jenssen <emil.jenssen@brilliant.no>
2024-04-15 13:42:11 +0200

    Removed unused file(s)

--- docker-wsl-usb/RUN.bat
+++ docker-wsl-usb/RUN.bat
@@ -24,13 +24,6 @@ echo.
 
 call windows-usbipd-usb-to-wsl.bat
 
-REM echo.
-REM echo ================================================================================
-REM echo Running windows-open-wsl-check-usb.bat
-REM echo ================================================================================
-REM call windows-open-wsl-check-usb.bat
-REM echo.
-
 echo.
 echo ================================================================================
 echo All scripts have been executed.
